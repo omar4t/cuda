5f18d19dd777266637fa63ae1dd63663e4032687
omar4t <mdomarali777@gmail.com>
2017-07-02 08:17:20 +0600

    Cuda HTML

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+/* ============================= This code for Active SlickNav Menu ================================== */
+
+$(document).ready(function(){
+	$('#nav').slicknav();
+});
+
+
+
+/* ============================= This code for Active MixItUp ================================== */
+
+
+    $(window).load(function() {
+        $('.portfolio_list').mixItUp();
+    });
+
